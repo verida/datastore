827e5278a5a58e157f154650894c738126095394
Chris Were <chris@mozzler.com.au>
2020-01-14 16:06:29 +1030

    Add keyring with unit tests

---
+++ keyring.js
@@ -0,0 +1,169 @@
+/*eslint no-console: "off"*/
+import { secretbox, box, sign, randomBytes } from "tweetnacl";
+import {
+  decodeUTF8,
+  encodeUTF8,
+  encodeBase64,
+  decodeBase64
+} from "tweetnacl-util";
+import { ethers } from 'ethers';
+
+const newSymNonce = () => randomBytes(secretbox.nonceLength);
+const newAsymNonce = () => randomBytes(box.nonceLength);
+
+const BASE_PATH = "m/6696500'/0'/0'"
+//const ETH_PATH = "m/44'/60'/0'/0"
+
+/**
+ * Container for user's encryption keys
+ */
+class Keyring {
+
+    /**
+     * 
+     * @param {string} seed Hex string of the seed generated from a message signed by the users's on chain account
+     */
+    constructor(seed) {
+        seed = Buffer.from(seed, 'hex');
+        const seedNode = ethers.HDNode.fromSeed(seed);
+        const baseNode = seedNode.derivePath(BASE_PATH);
+
+        // Build symmetric key
+        let symKey = baseNode.derivePath("0");
+        this.symKey = Buffer.from(symKey.privateKey.slice(2), 'hex');
+
+        // Build asymmetric keys
+        let asymKey = baseNode.derivePath("1");
+        this.asymKey = this._generateKeyPair(asymKey, "box");
+
+        // Build signing keys
+        let signKey = baseNode.derivePath("2");
+        this.signKey = this._generateKeyPair(signKey, "sign");
+
+        // Build auth keys
+        let authKey = baseNode.derivePath("3");
+        this.authKey = this._generateKeyPair(authKey, "box");
+
+        //this.ethKey = seedNode.derivePath(ETH_PATH);
+    }
+
+    exportPublicKeys() {
+        return {
+            asymmetric: this.asymKey.public,
+            asymmetricBytes: decodeUTF8(this.asymKey.public),
+            sign: this.signKey.public,
+            signBytes: decodeUTF8(this.signKey.public),
+            auth: this.authKey.public,
+            authBytes: decodeUTF8(this.authKey.public),
+        }
+    }
+
+    _generateKeyPair(hdNode, method) {
+        let seed = Buffer.from(hdNode.privateKey.slice(2), 'hex');
+        
+        let keyPair;
+        switch (method) {
+            case 'sign':
+                keyPair = sign.keyPair.fromSeed(seed);
+                break;
+            case 'box':
+                keyPair = box.keyPair.fromSecretKey(seed)
+                break;
+        }
+
+        return {
+            public: Buffer.from(keyPair.publicKey).toString('hex'),
+            publicBytes: keyPair.publicKey,
+            private: Buffer.from(keyPair.secretKey).toString('hex'),
+            privateBytes: keyPair.secretKey,
+        };
+    }
+
+    // get a signature
+    sign(data) {
+        let messageUint8 = decodeUTF8(JSON.stringify(data));
+        return encodeBase64(sign.detached(messageUint8, this.signKey.privateBytes));
+    }
+
+    verifySig(data, sig) {
+        let messageUint8 = decodeUTF8(JSON.stringify(data));
+        return sign.detached.verify(messageUint8, decodeBase64(sig), this.signKey.publicBytes);
+    }
+
+    symEncrypt(data) {
+        const keyUint8Array = this.symKey;
+
+        const nonce = newSymNonce();
+        const messageUint8 = decodeUTF8(JSON.stringify(data));
+        const box = secretbox(messageUint8, nonce, keyUint8Array);
+
+        const fullMessage = new Uint8Array(nonce.length + box.length);
+        fullMessage.set(nonce);
+        fullMessage.set(box, nonce.length);
+
+        const base64FullMessage = encodeBase64(fullMessage);
+        return base64FullMessage;
+    }
+
+    symDecrypt(messageWithNonce) {
+        const keyUint8Array = this.symKey;
+        const messageWithNonceAsUint8Array = decodeBase64(messageWithNonce);
+        const nonce = messageWithNonceAsUint8Array.slice(0, secretbox.nonceLength);
+        const message = messageWithNonceAsUint8Array.slice(
+            secretbox.nonceLength,
+            messageWithNonce.length
+        );
+
+        const decrypted = secretbox.open(message, nonce, keyUint8Array);
+        if (!decrypted) {
+            throw new Error("Could not decrypt message");
+        }
+
+        const base64DecryptedMessage = encodeUTF8(decrypted);
+        return JSON.parse(base64DecryptedMessage);
+    }
+
+    asymEncrypt(data, secretOrSharedKey) {
+        secretOrSharedKey = secretOrSharedKey ? secretOrSharedKey : this.asymKey.publicBytes;
+        const nonce = newAsymNonce();
+        const messageUint8 = decodeUTF8(JSON.stringify(data));
+        const encrypted = box.after(messageUint8, nonce, secretOrSharedKey);
+
+        const fullMessage = new Uint8Array(nonce.length + encrypted.length);
+        fullMessage.set(nonce);
+        fullMessage.set(encrypted, nonce.length);
+
+        const base64FullMessage = encodeBase64(fullMessage);
+        return base64FullMessage;
+    }
+      
+    asymDecrypt(messageWithNonce, secretOrSharedKey) {
+        secretOrSharedKey = secretOrSharedKey ? secretOrSharedKey : this.asymKey.privateBytes;
+        const messageWithNonceAsUint8Array = decodeBase64(messageWithNonce);
+        const nonce = messageWithNonceAsUint8Array.slice(0, box.nonceLength);
+        const message = messageWithNonceAsUint8Array.slice(
+            box.nonceLength,
+            messageWithNonce.length
+        );
+
+        const decrypted = box.open.after(message, nonce, secretOrSharedKey);
+
+        if (!decrypted) {
+            throw new Error('Could not decrypt message');
+        }
+
+        const base64DecryptedMessage = encodeUTF8(decrypted);
+        return JSON.parse(base64DecryptedMessage);
+    }
+
+    buildSharedKeyStart(privateKey) {
+        return box.before(this.asymKey.publicBytes, privateKey);
+    }
+
+    buildSharedKeyEnd(publicKey) {
+        return box.before(publicKey, this.asymKey.privateBytes);
+    }
+
+}
+
+export default Keyring;
---
+++ test/keyring.js
@@ -0,0 +1,62 @@
+var assert = require("assert");
+import { box } from "tweetnacl";
+
+import Keyring from "../keyring";
+import { decodeUTF8 } from "tweetnacl-util";
+
+describe("Keyring", function() {
+    var keyring;
+
+    describe("Signing", function() {
+        this.beforeAll(function() {
+            let seed = new Uint8Array([37, 111, 93, 122, 134, 74, 12, 201, 10, 204, 68, 90, 213, 69, 150, 82, 163, 69, 157, 239, 64, 194, 140, 31, 144, 79, 225, 141, 74, 52, 70, 90]);
+            seed = Buffer.from(seed).toString('hex');
+            keyring = new Keyring(seed);
+        });
+
+        it("should generate a verified signature", function() {
+            let data = {
+                "hello": "world"
+            };
+
+            let sig = keyring.sign(data);
+            let verified = keyring.verifySig(data, sig);
+            
+            assert(verified,true);
+        });
+    });
+
+    describe("Symmetric encryption", function() {
+        it("should symmetrically encrypt and decrypt", function() {
+            let data = {
+                "hello": "world"
+            };
+
+            let encryptedMessage = keyring.symEncrypt(data);
+            let decryptedMessage = keyring.symDecrypt(encryptedMessage);
+
+            assert.deepEqual(data, decryptedMessage);
+        });
+    });
+
+    describe("Asymmetric encryption", function() {
+        it("should assymetrically encrypt and decrypt an inbox message", function() {
+            let data = {
+                "hello": "world"
+            };
+    
+            // create a test key pair representing the user submitting to an inbox
+            let otherKeyPair = box.keyPair();
+            
+            // encrypt the data
+            let sharedKeyStart = keyring.buildSharedKeyStart(otherKeyPair.secretKey);
+            let encrypted = keyring.asymEncrypt(data, sharedKeyStart);
+
+            // decrypt the data
+            let sharedKeyEnd = keyring.buildSharedKeyEnd(otherKeyPair.publicKey);
+            let decrypted = keyring.asymDecrypt(encrypted, sharedKeyEnd);
+    
+            assert.deepEqual(data, decrypted);
+        });
+    });
+});
