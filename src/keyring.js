edf089ea87cc00193b6f9ad86393e057203883ed
Chris Were <chris@mozzler.com.au>
2020-02-19 22:39:02 +1030

    Support encrypt / decrypt buffer.

--- src/keyring.js
+++ src/keyring.js
@@ -85,6 +85,38 @@ class Keyring {
         return sign.detached.verify(messageUint8, decodeBase64(sig), this.signKey.publicBytes);
     }
 
+    symEncryptBuffer(data) {
+        const keyUint8Array = this.symKey;
+
+        const nonce = newSymNonce();
+        const messageUint8 = data;
+        const box = secretbox(messageUint8, nonce, keyUint8Array);
+
+        const fullMessage = new Uint8Array(nonce.length + box.length);
+        fullMessage.set(nonce);
+        fullMessage.set(box, nonce.length);
+
+        const base64FullMessage = encodeBase64(fullMessage);
+        return base64FullMessage;
+    }
+
+    symDecryptBuffer(messageWithNonce) {
+        const keyUint8Array = this.symKey;
+        const messageWithNonceAsUint8Array = decodeBase64(messageWithNonce);
+        const nonce = messageWithNonceAsUint8Array.slice(0, secretbox.nonceLength);
+        const message = messageWithNonceAsUint8Array.slice(
+            secretbox.nonceLength,
+            messageWithNonce.length
+        );
+
+        const decrypted = secretbox.open(message, nonce, keyUint8Array);
+        if (!decrypted) {
+            throw new Error("Could not decrypt message");
+        }
+
+        return decrypted;
+    }
+
     symEncrypt(data) {
         const keyUint8Array = this.symKey;
 
