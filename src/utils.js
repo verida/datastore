760e887381bc3f242638ff638aeaf1eec5114721
Chris Were <chris@mozzler.com.au>
2020-03-21 11:35:25 +1030

    Add md5 hash function from an array.

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,20 @@
+import crypto from 'crypto';
 
 class Utils {
     sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    /**
+     * Build an MD5 hash from an array
+     * 
+     * @param {array} parts Array of components to build the hash
+     */
+    md5FromArray(parts) {
+        let text = parts.join("/");
+        console.log(text);
+
+        return crypto.createHash('md5').update(text).digest("hex");
     }
 }
 
