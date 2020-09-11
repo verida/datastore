1aa009b0600441f0658f96305ca70316be55c0e7
Chris Were <chris@mozzler.com.au>
2020-09-12 07:31:28 +0930

    Add compiled build support

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'verida-datastore.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Verida',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-transform-runtime', {
+                'regenerator': true
+              }],
+              ['@babel/plugin-proposal-object-rest-spread']
+            ]
+          }
+        }
+      }
+    ]
+  },
+  node: {
+    console: false,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  }
+};
